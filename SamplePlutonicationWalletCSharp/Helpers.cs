f9d597e5ec1a6dda1b3a0c719354c16ef73ffbe3
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-10-31 12:28:05 +0100

    Base

---
+++ SamplePlutonicationWalletCSharp/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Schnorrkel.Keys;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Types;
+
+namespace SamplePlutonicationWalletCSharp
+{
+	public class Helpers
+	{
+        private const string PRIVATE_KEY = "0x34847CDDAA7E3E253D6DC7B7BB1D819B6469B8256C28F46E8CC6D07DA91B240AD3B76034856F5DCB0DF96194ABFD0AF1E1202554C99565E5C65F86CB69BC6D9D";
+
+        public Helpers()
+		{
+		}
+
+		public static Account GetAccount()
+		{
+            var miniSecret = new MiniSecret(Utils.HexToByteArray(PRIVATE_KEY), ExpandMode.Ed25519);
+
+            return Account.Build(KeyType.Sr25519,
+                miniSecret.ExpandToSecret().ToBytes(),
+                miniSecret.GetPair().Public.Key);
+        }
+
+        public static uint HexStringToUint(string hex)
+        {
+            hex = hex.Replace("0x", ""); // remove the 0x if it's there
+            if (uint.TryParse(hex, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new FormatException("The provided string is not a valid hexadecimal number");
+            }
+        }
+    }
+}
+
---
+++ SamplePlutonicationWalletCSharp/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using Plutonication;
+using SamplePlutonicationWalletCSharp;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Extrinsics;
+using Substrate.NetApi.Model.Rpc;
+using Substrate.NetApi.Model.Types;
+using Substrate.NetApi.Model.Types.Base;
+
+Account account = Helpers.GetAccount();
+Console.WriteLine("Public key: " + account.Value);
+
+await PlutonicationWalletClient.InitializeAsync(
+    ac: new AccessCredentials(new Uri("plutonication:?key=1&url=wss://plutonication-acnha.ondigitalocean.app/")),
+    pubkey: account.Value,
+    signPayload: payloadJson =>
+    {
+        try
+        {
+            Console.WriteLine("Payload: ");
+            Console.WriteLine(payloadJson);
+
+            Plutonication.Payload payload = JsonConvert.DeserializeObject<Plutonication.Payload[]>(payloadJson.ToString())[0];
+
+            Console.WriteLine("Payload received, nice!");
+            Console.WriteLine(payload.method);
+
+            byte[] methodBytes = Utils.HexToByteArray(payload.method);
+
+            List<byte> methodParameters = new List<byte>();
+
+            for (int i = 2; i < methodBytes.Length; i++)
+            {
+                methodParameters.Add(methodBytes[i]);
+            }
+
+            Method method = new Method(methodBytes[0], methodBytes[1], methodParameters.ToArray());
+
+            Hash eraHash = new Hash();
+            eraHash.Create(Utils.HexToByteArray(payload.era));
+
+            Hash blockHash = new Hash();
+            blockHash.Create(payload.blockHash);
+
+            Console.WriteLine("HexEra: " + payload.era);
+            Console.WriteLine(eraHash);
+
+            Hash genesisHash = new Hash();
+            genesisHash.Create(Utils.HexToByteArray(payload.genesisHash));
+
+            RuntimeVersion runtime = new RuntimeVersion
+            {
+                ImplVersion = payload.version,
+                SpecVersion = Helpers.HexStringToUint(payload.specVersion),
+                TransactionVersion = Helpers.HexStringToUint(payload.transactionVersion),
+            };
+
+            var extrinsic = RequestGenerator.SubmitExtrinsic(
+                true,
+                account,
+                method,
+                Era.Decode(Utils.HexToByteArray(payload.era)),
+                Helpers.HexStringToUint(payload.nonce),
+                ChargeAssetTxPayment.Default(),
+                genesisHash,
+                blockHash,
+                runtime
+            );
+
+            var signerResult = new SignerResult
+            {
+                id = 10000,
+                signature = Utils.Bytes2HexString(extrinsic.Signature).ToLower(),
+            };
+
+            Console.WriteLine("Signature: " + Utils.Bytes2HexString(extrinsic.Signature).ToLower());
+
+            Console.WriteLine("Nearly done");
+
+            PlutonicationWalletClient.SendSignedPayloadAsync(signerResult);
+            Console.WriteLine("PAYLOAD sent");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("PlutonicationWallet error");
+            Console.WriteLine(ex.Message);
+
+        }
+    },
+    signRaw: raw =>
+    {
+        // To be added later...
+    });
+
+Console.WriteLine("Connected successfully");
+
+Console.ReadKey();
+
+Console.WriteLine("Bye bye.");
